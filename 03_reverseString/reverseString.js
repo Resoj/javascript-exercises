272a638b495c26084acb5a0df595e4fd93c8a252
Jose Cruz <resoj.cruz@gmail.com>
2024-01-10 11:31:08 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    arr = string.split('');
+    res = [];
+    for(let i = arr.length - 1; i >= 0; i--){
+        res.push(arr[i]);
+    }
+    return res.join('');
 };
 
 // Do not edit below this line
