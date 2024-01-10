cbddfd39fc895927c8aa4641ade5e30bad3a5e95
Jose Cruz <resoj.cruz@gmail.com>
2024-01-10 12:01:39 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
 
+    for(let i = 1; i < arguments.length; i++){
+        if(arr.includes(arguments[i])){
+            let index = arr.indexOf(arguments[i]);
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
