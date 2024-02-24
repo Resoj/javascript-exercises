6a7054a0f80053cc8866fdfa192ee8a0808200ae
Jose Cruz <resoj.cruz@gmail.com>
2024-02-24 00:58:19 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num !== 'number') {
+        num = parseInt(num)
+    }
 
+    if(num < 0){
+        return 'OOPS'
+    }
+    if(num === 0){
+        return 0
+    }
+    prev = 1
+    current = 1
+
+    for(let i = 0; i < num - 2; i++){
+        temp = prev + current;
+        prev = current;
+        current = temp;
+    }
+    return current;
 };
 
 // Do not edit below this line
