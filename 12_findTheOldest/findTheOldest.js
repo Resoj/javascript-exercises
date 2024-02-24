9dfe0579552438fd29931fd1540604b69701269b
Jose Cruz <resoj.cruz@gmail.com>
2024-02-24 01:39:31 -0500

    Complete exercise 12 (final exercise)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((person1, person2) => {
+        let lastGuy = null;
+        let nextGuy = null;
+        if(person1.yearOfDeath != undefined){
+            lastGuy = person1.yearOfDeath - person1.yearOfBirth;
+        }
+        else{
+            lastGuy = (new Date()).getFullYear() - person1.yearOfBirth;
+        }
+        if(person2.yearOfDeath != undefined){
+            nextGuy = person2.yearOfDeath - person2.yearOfBirth;
+        }
+        else{
+            nextGuy = (new Date()).getFullYear() - person2.yearOfBirth;
+        }
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
